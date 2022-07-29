3a6294283e5d7cf8686098e2d9366d2dc37e76e4
skassaye <104604921+skassaye@users.noreply.github.com>
2022-07-29 09:29:35 -0700

    initial commit

---
+++ itemsApi/src/test/java/com/example/itemsApi/ItemsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itemsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
